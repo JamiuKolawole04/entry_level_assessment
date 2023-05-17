e13249a19cf35eced4f5b1266d77b29ab3ae0c42
JamiuKolawole04 <Jakko4real@gmail.com>
2023-05-17 03:52:44 +0100

    chore: filtering base on request query short title

--- src/controllers/session.controller.ts
+++ src/controllers/session.controller.ts
@@ -4,17 +4,28 @@ import { getSessions } from "../api";
 import AppError from "../utils/appError";
 import { Session } from "../api";
 
+interface ResponseObj {
+  success: boolean;
+  message: string;
+  sessions: Session[];
+}
+
 export const sessions = async (
-  req: Request<
-    { short_title?: string; status?: string },
-    { sucess: boolean; message: string; sessions: Session },
-    {}
-  >,
-  res: Response,
+  req: Request<{}, ResponseObj, {}, { short_title?: string; status?: string }>,
+  res: Response<ResponseObj>,
   next: NextFunction
 ) => {
   try {
-    const sessions = await getSessions();
+    let sessions = await getSessions();
+    const { short_title, status } = req.query;
+
+    if (short_title) {
+      sessions = sessions.filter((el) => {
+        return el.program.some((program) => {
+          return program.short_title === short_title;
+        });
+      });
+    }
 
     res.status(200).json({
       success: true,
